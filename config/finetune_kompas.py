ad0f683b84792c907906fc7440c35bf5831ebfd6
dayat81 <hiidaayaat@gmail.com>
2024-08-10 00:31:11 +0000

    test finetuning

--- config/finetune_kompas.py
+++ config/finetune_kompas.py
@@ -1,19 +1,20 @@
-# train a miniature character-level shakespeare model
-# good for debugging and playing on macbooks and such
+import time
 
 out_dir = 'out-kompas-char'
 eval_interval = 250 # keep frequent because we'll overfit
 eval_iters = 200
 log_interval = 10 # don't print too too often
+wandb_log = False # feel free to turn on
+wandb_project = 'tempo-char'
+wandb_run_name = 'ft-' + str(time.time())
 
-# we expect to overfit on this small dataset, so only save when val improves
+dataset = 'tempo_char'
+init_from = 'resume' # this is the largest GPT-2 model
+
+# only save checkpoints if the validation loss improves
 always_save_checkpoint = False
 
-wandb_log = False # override via command line if you like
-wandb_project = 'kompas-char'
-wandb_run_name = 'mini-gpt'
 
-dataset = 'kompas_char'
 gradient_accumulation_steps = 1
 batch_size = 64
 block_size = 256 # context of up to 256 previous characters
@@ -25,8 +26,8 @@ n_embd = 384
 dropout = 0.2
 
 learning_rate = 1e-3 # with baby networks can afford to go a bit higher
-max_iters = 5000
-lr_decay_iters = 5000 # make equal to max_iters usually
+max_iters = 10000
+lr_decay_iters = 10000 # make equal to max_iters usually
 min_lr = 1e-4 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
 
